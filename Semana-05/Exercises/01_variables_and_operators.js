456a709042ce6a5e815ad721193984c548708d91
Sebastian Sosa <seba.r888@gmail.com>
2022-04-15 16:58:28 -0300

    BaSP-W05-2022:01_variables_and_operators READY

---
+++ Semana-05/Exercises/01_variables_and_operators.js
@@ -0,0 +1,42 @@
+console.log('01_variables_and_operators')
+/**** START *****/
+
+/* Excercise A */
+
+var inflacionMarzo, inflacionFebrero;
+
+inflacionFebrero = 3;
+inflacionMarzo = 6;
+sumaInflacion = inflacionFebrero + inflacionMarzo;
+
+console.log (sumaInflacion);
+
+/* Excercise B */
+
+var A, B;
+
+A = 'Hola Seba';
+B = 'lindo dia';
+C = A + B;
+
+console.log (C);
+
+/* Excercise C */
+
+var country, city;
+
+country = 'Dinamarca'
+city = 'Copenhague'
+numberOfLetters = country.length + city.length;
+
+console.log(numberOfLetters);
+
+/**** END *****/
+
+
+
+
+
+
+
+
