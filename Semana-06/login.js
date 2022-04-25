c501aee24f616e46ee246203c77c671783c0bc31
Sebastian Sosa <seba.r888@gmail.com>
2022-04-25 09:01:27 -0300

    Modified-CSS

--- Semana-06/login.js
+++ Semana-06/login.js
@@ -1,37 +1,4 @@
-/*
-var email,password
 
-email=/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
-
-password=/^.{4,12}$/// 4 a 12 digitos
-
-*/
-
-
-
-/*
-var inputPassword = document.getElementById('password');
-
-function validatePassword (){
-    var letterSum = 0;
-    var numberSum = 0;
-
-    if (inputPassword.value.length < 8){
-        return true;
-    }   else{
-        for (i=0; i < inputPassword.value.length; i++){
-            if (isNaN(inputPassword.value[i])){
-                letterSum += 1;
-            }   else{
-                numberSum += 1;
-            }
-        }
-    }
-
-    return  (letterSum === 0 || numberSum === 0)
-}
-
-*/
 /*
 const password = document.querySelector('input[type="password"]');
 
@@ -43,11 +10,37 @@ password.addEventListener('focus', (event) => {
   });
 
 */
-  
+
+window.onload = function(){
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+    var message = document.getElementsByClassName("message");
+    var format = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+
+    console.log(email);
+    console.log(password);
+    console.log(message);
+
+    function validateEmail (){
+        if (email.value.match(format)){
+            message[0].classList.remove('hide');
+            message[0].classList.add('error');
+            message[0].innerHTML = "ERROR";
+        } else if (email.value.match(format)) {
+            message[0].classList.remove('hide');
+            message[0].classList.remove('error');
+            message[0].classList.add('correct');
+            message[0].innerHTML = "CORRECT";
+        }
+    }
 
 
+    email.onblur = function(){
+        email();
+    };
 
+    email.onfocus = function(){
+        message[0].classList.add('hide');
+    };
 
-
-var button = document.getElementById("button");
-console.log(button);
+}
